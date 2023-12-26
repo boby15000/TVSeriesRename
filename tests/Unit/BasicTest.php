3455d4290140339ea3786977177811cb5b42d4b3
Nicolas Fourgheon <boby15000@hotmail.com>
2023-12-26 21:21:21 +0400

    Création des fichiers de test

---
+++ tests/Unit/BasicTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function testSerie(): void
+    {
+        
+        
+        
+        
+        
+        
+        $this->assertTrue(true);
+    }
+}
